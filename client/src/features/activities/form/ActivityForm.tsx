ec887099cb0d1c099337276494ae92cac2131789
Jake Greer <jakegreer11@gmail.com>
2020-01-28 18:04:17 -0800

    created mobx activity store and linked components

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,23 +1,20 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useContext } from 'react';
 import { Segment, Form, Button } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
+import ActivityStore from '../../../app/stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 interface IProps {
-    setEditMode: (editMode: boolean) => void;
     activity: IActivity;
-    createActivity: (activity: IActivity) => void;
-    editActivity: (activity: IActivity) => void;
-    submitting: boolean;
 }
 
-export const ActivityForm: React.FC<IProps> = ({ 
-    setEditMode, 
+const ActivityForm: React.FC<IProps> = ({
     activity: initialFormState,
-    createActivity,
-    editActivity,
-    submitting
 }) => {
+    const activityStore = useContext(ActivityStore);
+    const {createActivity, editActivity, cancelFormOpen, submitting} = activityStore;
+
     const initializeForm = () => {
         if (initialFormState) {
             return initialFormState;
@@ -107,9 +104,11 @@ export const ActivityForm: React.FC<IProps> = ({
                     floated='right' 
                     type='button' 
                     content='Cancel' 
-                    onClick={() => setEditMode(false)} 
+                    onClick={cancelFormOpen} 
                 />
             </Form>
         </Segment>
     )
 }
+
+export default observer(ActivityForm);
