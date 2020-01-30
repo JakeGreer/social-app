9433d29e2ec76116e5240844424b9aeededc0b67
Jake Greer <jakegreer11@gmail.com>
2020-01-29 17:02:27 -0800

    added semantic ui styles

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,11 +15,11 @@ namespace API.Controllers
         private readonly IMediator _mediator;
         public ActivitiesController(IMediator mediator)
         {
-            this._mediator = mediator;
+            _mediator = mediator;
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List() 
+        public async Task<ActionResult<List<Activity>>> List()
         {
             return await _mediator.Send(new List.Query());
         }
@@ -27,7 +27,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
-            return await _mediator.Send(new Details.Query{Id = id});
+            return await _mediator.Send(new Details.Query { Id = id });
         }
 
         [HttpPost]
@@ -36,18 +36,17 @@ namespace API.Controllers
             return await _mediator.Send(command);
         }
 
-        [HttpPut("{Id}")]
+        [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
         {
             command.Id = id;
             return await _mediator.Send(command);
         }
 
-        [HttpDelete("{Id}")]
-
+        [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            return await _mediator.Send(new Delete.Command{Id = id});
+            return await _mediator.Send(new Delete.Command { Id = id });
         }
     }
 }
